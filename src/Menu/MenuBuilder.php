6f17b52fb7e6e6294d06149f3fb1a24319c70192
AlexJanss <alex.janssen20@gmail.com>
2020-07-07 20:03:37 +0200

    edit knp menu service to add css classes to root element

--- src/Menu/MenuBuilder.php
+++ src/Menu/MenuBuilder.php
@@ -20,10 +20,18 @@ class MenuBuilder
 
     public function createMainMenu(array $options): ItemInterface
     {
-        $menu = $this->factory->createItem('root');
+        $menu = $this->factory->createItem('root', [
+            'childrenAttributes' => [
+                'class' => 'navbar-nav',
+            ]
+        ]);
 
         $menu->addChild('Home', ['route' => 'default']);
+        $menu['Home']->setAttribute('class', 'navbar-item');
+        $menu['Home']->setLinkAttribute('class', 'nav-link');
         $menu->addChild('Weather Data', ['route' => 'weather_data_index']);
+        $menu['Weather Data']->setAttribute('class', 'navbar-item');
+        $menu['Weather Data']->setLinkAttribute('class', 'nav-link');
 
         return $menu;
     }
